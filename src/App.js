7cbc1393db43774fc34bc4e6854f0ea8a81a394f
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-04-19 19:48:45 +0300

    All components and state hooks added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Modal from './components/Modal/Modal';
+import UserForm from './components/userform/UserForm';
+import UsersList from './components/UsersList/UsersList';
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [activeModal, setActiveModal] = useState(false);
+  const [message, setMessage] = useState({ title: '', msg: '' });
+
+  const handleSubmit = (submitData) => {
+    const { user, hasError, error } = submitData;
+
+    if (!hasError) {
+      setUsers((prevUsers) => [user, ...prevUsers]);
+    } else {
+      setActiveModal(true);
+      setMessage(error);
+    }
+  };
+
+  const handleDelete = (id) => {
+    setUsers((prevUsers) => {
+      return prevUsers.filter((user) => user.id !== id);
+    });
+  };
+
+  const closeModal = () => {
+    setActiveModal(false);
+    setMessage({ title: '', message: '' });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <UserForm onSubmit={handleSubmit} />
+      <UsersList userslist={users} onDelete={handleDelete} />
+      <Modal isActive={activeModal} message={message} onClick={closeModal} />
     </div>
   );
-}
+};
 
 export default App;
